35b89962ebb411eabee1e0cf5ffb30ab4e0fa22e
Jordan Elias <136925465+JORD4NELIAS@users.noreply.github.com>
2023-06-17 22:25:24 -0300

    update_3
    Mudança de arquivo .json e acrescido de dados de outra nuvem

---
+++ pointclouds/potree_converted/cloud.js
@@ -0,0 +1,99 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 278599,
+    "boundingBox": {
+        "lx": 450440.69,
+        "ly": 7543746.99,
+        "lz": 347.83,
+        "ux": 451837.64,
+        "uy": 7545143.94,
+        "uz": 1744.7800000000116
+    },
+    "tightBoundingBox": {
+        "lx": 450440.69,
+        "ly": 7543746.99,
+        "lz": 347.83,
+        "ux": 451837.64,
+        "uy": 7544826.52,
+        "uz": 436.28
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "normal x",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal y",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal z",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        }
+    ],
+    "spacing": 12.097942352294922,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
